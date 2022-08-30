e945d217905bc0623842af26fab7cac190b40364
Vasil Yordanov <56123202+vsyordanov@users.noreply.github.com>
2022-08-30 15:23:34 +0200

    center map changed

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -8,10 +8,10 @@ $(() => app = new App());
 class App {
 
     /** The default coordinates of the center of the map. */
-    static get defaultCoords() { return [9.679417, 46.106759] }
+    static get defaultCoords() { return [9.8276,46.1946] }
 
     /** The default zoom of the map. */
-    static get defaultZoom() { return 13 }
+    static get defaultZoom() { return 10 }
 
 
     /**
